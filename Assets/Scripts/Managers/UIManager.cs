b48392dd27354bf6ddc7989ade211151978b19ed
LuzMurilo <murilo.stucki@gmail.com>
2024-07-02 19:02:56 -0300

    created player life points
    player have 3 hearts, they lose 1 when hit a small rock, dies when loses all

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -8,6 +8,7 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Singleton {get; private set;}
     [SerializeField] private TMP_Text coinDisplay;
+    [SerializeField] private List<GameObject> hearts;
 
     private void Awake() {
         if (UIManager.Singleton != null)
@@ -18,8 +19,23 @@ public class UIManager : MonoBehaviour
         UIManager.Singleton = this;
 
     }
+
     public void SetCoinDisplayNumber(int coins)
     {
         coinDisplay.text = coins.ToString();
+    }
+
+    public void SetHeartsDisplayed(int number)
+    {
+        if (number > hearts.Count)
+        {
+            Debug.LogError("[UI] not enough heart objects on List!");
+            return;
+        }
+        hearts.ForEach(heart => heart.SetActive(false));
+        for (int i = 0; i < number; i++)
+        {
+            hearts[i].SetActive(true);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -7,10 +7,13 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private PlayerCollision playerCollision;
-
-    public int totalCoins;
-    public int blocksTraveled;
-    public bool isInvulnerable;
+    [SerializeField] private int maxLife;
+    
+    public int currentLife {get; private set;}
+    public int totalCoins {get; private set;}
+    public int blocksTraveled {get; private set;}
+    public bool isInvulnerable {get; private set;}
+    public bool isDead {get; private set;}
 
     public UnityEvent OnPlayerDeath;
     public UnityEvent OnTakeHit;
@@ -18,22 +21,33 @@ public class PlayerManager : MonoBehaviour
     public void StartPlayer()
     {
         isInvulnerable = false;
+        isDead = false;
         totalCoins = 0;
         blocksTraveled = 0;
+        currentLife = maxLife;
+        UIManager.Singleton.SetHeartsDisplayed(currentLife);
         playerMovement.StartMovement();
     }
 
     public void Die()
     {
+        if (isDead) return;
         Debug.Log("[Player] Player Died!");
-
+        isDead = true;
         OnPlayerDeath.Invoke();
     }
 
     public void TakeHit()
     {
-        if (isInvulnerable) return;
+        if (isInvulnerable || isDead) return;
         Debug.Log("Took hit!");
+        currentLife--;
+        UIManager.Singleton.SetHeartsDisplayed(currentLife);
+        if (currentLife <= 0)
+        {
+            Die();
+            return;
+        }
         OnTakeHit.Invoke();
         StartCoroutine(InvulnerableFrame(1.0f));
     }
